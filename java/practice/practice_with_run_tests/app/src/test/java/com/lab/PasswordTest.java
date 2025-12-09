4d4792b301ff949bb154185d1f056f37a0681200
Dragos Mihai Zugrav <dz222cr@student.lnu.se>
2025-12-09 19:05:10 +0100

    Finished the tests on first part

--- java/practice/practice_with_run_tests/app/src/test/java/com/lab/PasswordTest.java
+++ java/practice/practice_with_run_tests/app/src/test/java/com/lab/PasswordTest.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordTest {
     private IPassword getPassword(String s) throws Exception {
-        return (IPassword) new Password(s);
+        // return (IPassword) new Password(s);
         // return (IPassword) new BugDoesNotTrim(s);
         // return (IPassword) new BugToShortPassword(s);
         // return (IPassword) new BugToShortPassword(s);
@@ -33,11 +33,79 @@ public class PasswordTest {
         // return (IPassword) new BugMissingPasswordLengthCheck(s);
         // return (IPassword) new BugMissingNumberCheck(s);
         // return (IPassword) new BugIsPasswordSameAlwaysTrue(s);
-        // return (IPassword) new BugWrongHashingAlgorithm(s);
+         return (IPassword) new BugWrongHashingAlgorithm(s);
     }
 
     @Test
-    public void shouldAlwaysPass() throws Exception {
-        assertTrue(true);
+    public void trimTest() throws Exception {
+        String noTrim = " 1234567890 ";
+
+        assertThrows(Exception.class, () -> {
+            getPassword(noTrim);
+        }, "Expected exception because password is not trimed");
+    }
+
+    @Test
+    public void Passlength() throws Exception {
+        String shortPass = "12345678901";
+        
+        assertThrows(Exception.class, () -> {
+            getPassword(shortPass);
+        }, "Expected exception because password is less than 12 chars");
+    }
+
+    @Test
+    public void veryShort() throws Exception {
+        String shorterPass = "123456";
+
+        assertThrows(Exception.class, () -> {
+            getPassword(shorterPass);
+        }, "Expected exception because password is less than 6 chars");
+    }
+
+    @Test
+    public void Wrongexception() {
+        String shortPass = "12345678901";
+        Exception exception = assertThrows(Exception.class, () -> {
+            getPassword(shortPass);
+        }, "Expected exception because password is less then 12 chars");
+
+        String correctMessage = "To short password";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(correctMessage, actualMessage, "Message should match the requirement!");
+    }
+
+    @Test
+    public void lengthCheck() throws Exception {
+        String shortPass = "12345678901";
+        assertDoesNotThrow(() -> {
+            getPassword(shortPass);
+        }, "This password should of had an exception thrown but none was presented");
+    }
+
+    @Test
+    public void numCheck() throws Exception {
+        String noNum = "HahaNoNumber";
+
+        assertThrows(Exception.class, () -> {
+            getPassword(noNum);
+        },"Expected exception since password has no number");
+    }
+
+    @Test
+    public void duplicateTrue() throws Exception {
+        String pass = "123456789012";
+        String pass2 = "123456789013";
+        IPassword og = getPassword(pass);
+        assertFalse(og.isPasswordSame(getPassword(pass2)), "Password should be false");
+    }
+
+    @Test
+    public void hashError() throws Exception {
+        String pass = "12345678901A";
+        String pass2 = "12345678901F";
+        IPassword p1 = getPassword(pass);
+        assertFalse(p1.isPasswordSame(getPassword(pass2)), "Passwords are different, buggy hash");
     }
 }
